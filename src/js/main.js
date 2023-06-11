acbde461f9d72754d8e8de9b9eff98efec931c62
SaraOsacar <saraosacar@gmail.com>
2023-06-11 22:36:36 +0200

    Add event and new render function

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,13 @@
 
 // Query Selector
 const charactersList = document.querySelector('.js_listCharacters');
-const urlCharacters = `https://api.disneyapi.dev/character?page=50`;
+const urlCharacters = `https://api.disneyapi.dev/character?pageSize=50`;
 const input = document.querySelector('.js_descSearch');
 const btnSearch = document.querySelector('.js_btnSearch');
 const favourites = document.querySelector('.js_listFavourites');
 
 let listCharactersData = [];
+let listFavouritesCharacters = [];
 
 
 // 1.Fetch para obtener información del servidor
@@ -25,15 +26,38 @@ function renderListCharacters (list) {
   for (const eachCharacter of list ) {
     charactersList.innerHTML += renderOneCharacter(eachCharacter);
   }
+  addEventFavouritesCharacters();
+}
+//renderiza lista de favoritos
+function renderFavoriteCards (list) {
+  for (const eachCharacter of list ) {
+    favourites.innerHTML += renderOneCharacter(eachCharacter);
+  }
 }
 
-// 3. Crear función que renderiza un personaje con la info que seleccione
+// 3. Crear función que renderiza un personaje del total de la lista con la info que seleccione
 function renderOneCharacter(dataCharacter) {
+  const defaultImg = 'https://via.placeholder.com/210x295/ffffff/555555/?text=Disney'; // creo constante por si la card no tuviera imagen, se le añada la que le metemos del placeh.
+  let cardImg = dataCharacter.imageUrl;
+  if (cardImg === ''){
+    cardImg = defaultImg;
+  }
+
   let html = `<li id="${dataCharacter.__id}">
                 <article>
-                <img src="${dataCharacter.imageUrl}" alt=${dataCharacter.name}/>
+                <img src="${cardImg}" alt=${dataCharacter.name}/>
                 <p js_name">${dataCharacter.name}</p>
                 </article>
             </li>`;
   return html;
 }
+
+//4. Generar el evento en cada character
+function addEventFavouritesCharacters () {
+  for(const singleCharacter of charactersList) {
+    singleCharacter.addEventListener('click', handleClick);
+  }
+}
+
+
+
